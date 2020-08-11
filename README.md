be468e13491733521a568186a12db3de0360c539
Charvi04 <66305846+Charvi04@users.noreply.github.com>
2020-08-11 12:04:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Pluck-The-Mangoes
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,96 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var tree, stone,ground, launcherObject;
+var mango1,mango2,mango3,mango4,mango5;
+var boy,boyImg;
+var score = 0;
+
+function preload()
+{
+	boyImg = loadImage("Images/boy.png");
+	backgroundImg = loadImage("Images/back.jpg")
+}
+
+function setup() {
+	createCanvas(1350, 600);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	boy = createSprite(190,530);
+	boy.addImage(boyImg);
+	boy.scale = 0.4;
+	tree = new Tree(900,350,30,300);
+	ground = new Ground(600,600,2000,20);
+	mango1 = new Mango(900,250,15);
+	mango2 = new Mango(800,200,15);
+	mango3 = new Mango(800,280,15);
+	mango4 = new Mango(1000,250,15);
+	mango5 = new Mango(670,300,15);
+	stone = new Stone(150,550,15);
+	boyShot = new Shot(stone.body,{x:150,y:500});
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  
+  Engine.update(engine);
+
+  background(backgroundImg);
+  tree.display();
+  ground.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  stone.display();
+  boyShot.display();
+  detectCollision(stone,mango1);
+  detectCollision(stone,mango2);
+  detectCollision(stone,mango3);
+  detectCollision(stone,mango4);
+  detectCollision(stone,mango5);
+  drawSprites();
+ textFont("Comic Sans MS");
+ textSize(30);
+ fill("magenta");
+ stroke(0);
+ strokeWeight(2);
+ text("Please help me in plucking the mangoes",20,50);
+ text(" Your Score : " + score,1100,50)
+}
+
+
+function mouseDragged(){
+    Matter.Body.setPosition(stone.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+    boyShot.fly();
+}
+function detectCollision(lstone,lmango){
+	mangoBodyPosition=lmango.body.position
+	stoneBodyPosition=lstone.body.position
+	var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+	if(distance<=lmango.r+lstone.r){
+		Matter.Body.setStatic(lmango.body,false);
+	}
+}
+
+function keyPressed(){
+
+	if(keyCode === 32){
+		Matter.Body.setPosition(stone.body,{x:150,y:550})
+		boyShot.attach(stone.body);
+	}
+}
